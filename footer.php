44729fbd029fe5ab6e6994342dec14f9fd70d0b7
Tim Booher <ads@theboohers.org>
2012-01-22 19:31:51 -0500

    removed footer

--- footer.php
+++ footer.php
@@ -38,7 +38,5 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
-
 </body>
 </html>
--- index.php
+++ index.php
@@ -49,3 +49,4 @@ get_header(); ?>
 		</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
+<?php get_footer(); ?>
