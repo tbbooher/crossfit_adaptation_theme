d2189c7bd22fdcf4dfe9d823957e0822f7064b73
Tim Booher <ads@theboohers.org>
2012-02-11 16:21:40 -0500

    updated

--- sidebar.php
+++ sidebar.php
@@ -43,8 +43,7 @@
       <p>in Shirlington, near Alexandria and DC</p>
     </div>
     <div class="teaser"> <a class="kettlebell" href="/paleo/"></a>
-      <h4><a title="Paleo Diet" href="/paleo/">Paleo Diet</a> | <a title="Paleo Challenge" href="/paleo-challenge/">Paleo Challenge</a>
-      </h4>
+      <h4><a title="Paleo Diet" href="/paleo/">Paleo Diet</a></h4>
       <p>fuel your work. take the challenge.</p>
     </div>
     <div class="teaser"> <a class="kettlebell" href="/contact_us/"></a>
