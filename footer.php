1b9748b5222c57428e6b103460505adf2f82b884
Tim Booher <tim@theboohers.org>
2013-09-26 14:55:35 -0400

    added jackson clinics

--- footer.php
+++ footer.php
@@ -22,18 +22,24 @@
       <div class="btn_adv">
         <a href="http://journal.crossfit.com/start.tpl?version=CFJ-graphic123x63" title="CrossFit Journal: The Performance-Based Lifestyle Resource"><img src="http://journal.crossfit.com/templates/images/graphic-125x63.jpg" width="125" height="63" alt="CrossFit Journal: The Performance-Based Lifestyle Resource" /></a>
       </div>
+      <!-- jackson clinics logo -->
       <div class="btn_adv">
+        <a href="http://www.thejacksonclinics.com/services/health/crossfit-program">
+          <img src="/wp-content/themes/crossfit_adaptation_theme/images/tjc_web_banner.jpg" alt="the jackson clinics" width="150" height="100" title="The Jackson Clinics" style="border:1px solid black;">
+        </a>
+      </div>   
+<!--       <div class="btn_adv">
           <a href="http://kids.crossfitfallschurch.com/">
             <img src="/wp-content/themes/crossfit_adaptation_theme/images/CFFC_kids_logo.jpg" width="150px" height="49px">
           </a>
-      </div>
+      </div> -->
       <div class="btn_adv">
         <a href="http://dc.mypowersupply.com/">
         <img src="/wp-content/themes/crossfit_adaptation_theme/images/powersupply-crest.png" alt="catalyst meals" width="170" height="69" title="Power Supply Meals" style="border:none;"></a>
-      </div>
+      </div>      
       <div class="btn_adv">
         <a href="http://www.arlingtonsportsmassage.com/">
-           <img src="/wp-content/uploads/2012/03/Erin_logo.jpg" alt="erin logo">
+           <img src="/wp-content/themes/crossfit_adaptation_theme/images/Erin_logo.jpg" alt="erin logo">
         </a>
       </div>
       <div class="btn_adv">
