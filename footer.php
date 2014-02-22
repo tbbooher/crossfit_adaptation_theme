130732c14e03b54bfd1fefd06a0b5d0a0639eb39
Tim Booher <tim@theboohers.org>
2014-02-22 05:10:22 -0500

    updated with keith

--- footer.php
+++ footer.php
@@ -54,7 +54,7 @@
           </a>       
       </div>
       <div class="btn_adv">
-          <a target="_blank" href="">
+          <a target="_blank" href="http://www.facebook.com/kxphotos">
              <img src="/wp-content/themes/crossfit_adaptation_theme/images/kx.png" alt="Cross Functional" style="border:none;">
           </a>       
       </div>      
